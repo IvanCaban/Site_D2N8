c9b6bee152e5fc9421f951ebcdcfa0f5125d5d48
IvanCaban <ivanheinlein@gmail.com>
2017-11-05 18:47:33 +0200

    5.11 anim sections

--- Script.js
+++ Script.js
@@ -34,13 +34,22 @@ headerAnim(2000);
 var sectionAnim = function(){
 	var titels = document.getElementsByClassName("section-title");
 	var descriptions = document.getElementsByClassName("section-desc");
+	var buttons = document.getElementsByClassName("section-button");
+	var allAnimElements = titels.length + descriptions.length + buttons.length;
+	var counter = 0;
 
-	window.addEventListener("scroll", function(e){
-		animeteSectionTitels(e);
-		animeteSectionDesc(e);
-	});
+	window.addEventListener("scroll", animeteSection);
 
-	function animeteSectionTitels(e){
+	function animeteSection(){
+		animeteSectionTitels();
+		animeteSectionDesc();
+		animeteSectionBtns();
+		//когда все анимировалось снимаю слушатель собития
+		if(counter >= allAnimElements){
+			window.removeEventListener("scroll", animeteSection);
+		}
+	}
+	function animeteSectionTitels(){
 		for(var i=0, len = titels.length; i<len; i++){
 			if(titels[i].classList.contains("section-title--showed")){
 				continue;
@@ -48,12 +57,13 @@ var sectionAnim = function(){
 				var cords = titels[i].getBoundingClientRect();
 				if(document.documentElement.clientHeight/2 >= cords.top){
 					titels[i].classList.add("section-title--showed");
+					counter++;
 				}
 			}
 		}
 	}
 
-	function animeteSectionDesc(e){
+	function animeteSectionDesc(){
 		for (var i = 0, len = descriptions.length; i<len; i++) {
 			if(descriptions[i].classList.contains("section-desc--showed")){
 				continue;
@@ -61,6 +71,21 @@ var sectionAnim = function(){
 				var cords = descriptions[i].getBoundingClientRect();
 				if(document.documentElement.clientHeight/2 >= cords.top){
 					descriptions[i].classList.add("section-desc--showed");
+					counter++;
+				}
+			}
+		}
+	}
+
+	function animeteSectionBtns(){
+		for (var i = 0, len = buttons.length; i<len; i++) {
+			if(buttons[i].classList.contains("section-button--showed")){
+				continue;
+			} else {
+				var cords = buttons[i].getBoundingClientRect();
+				if(document.documentElement.clientHeight/2 >= cords.top){
+					buttons[i].classList.add("section-button--showed");
+					counter++;
 				}
 			}
 		}
